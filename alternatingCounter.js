8da8e4a3d2d066d1d262610c0111ea2e36baf3f9
Akash11144 <82667535+Akash11144@users.noreply.github.com>
2021-06-10 20:33:15 -0700

    Add files via upload

---
+++ alternatingCounter.js
@@ -0,0 +1,19 @@
+var t=document.getElementById("cnt");
+var chk=0;
+var s=Number(t.innerText);
+if(s>0)
+    chk==0;
+else
+    chk==1;
+var i=window.setInterval(function(){
+    if(chk==0){
+        if(t.innerText==1)
+            chk=1;
+        --t.innerText;
+    }
+    else if(chk==1){
+        if(t.innerText>=9)
+            chk=0;
+        ++t.innerText;
+    }
+},1000);
